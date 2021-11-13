09c61f1aafe9379961fb7d11f9a54d6c2dda1f1b
Sanket, Wawale <sanket.wawale@gmail.com>
2021-11-13 20:27:10 +0530

    QA enviornment added

---
+++ QA/main.tf
@@ -0,0 +1,15 @@
+
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-0e5b6b6a9f3db6db8" 
+  instance_type = "t2.micro"
+  key_name = "Windows.pem"
+  subnet_id = "subnet-3030466d"
+  count = 1
+  user_data = file("script.sh")
+
+  tags = {
+    Name = "QA-Webserver"
+  }
+}
+
