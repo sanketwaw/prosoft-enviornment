43a905092c5bb813d587a52c07de68ad5d355321
Sanket, Wawale <sanket.wawale@gmail.com>
2021-11-13 21:42:48 +0530

    state file logic

--- QA/Provider.tf
+++ QA/Provider.tf
@@ -2,3 +2,11 @@ provider "aws"{
 version = "~> 3.0"
 region = "us-west-2"
 }
+
+terraform {
+backend "s3"{
+bucket = "enviornment-state-devops1"
+key = "QA/qa-state-file.tf"
+region = "us-west-2"
+}
+}
