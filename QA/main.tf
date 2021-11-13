f73bd357a694140b607becf48b222a3f9172635a
Sanket, Wawale <sanket.wawale@gmail.com>
2021-11-13 22:41:46 +0530

    state file logic

--- QA/main.tf
+++ QA/main.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "webserver" {
   ami           = "ami-0e5b6b6a9f3db6db8" 
   instance_type = "t2.micro"
-  #key_name = "Windows.pem"
+  key_name = "Windows"
   subnet_id = "subnet-3030466d"
   count = 1
   user_data = file("script.sh")
