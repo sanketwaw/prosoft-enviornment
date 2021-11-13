5cfa320c7d55e471001484b982db396be1274876
Sanket, Wawale <sanket.wawale@gmail.com>
2021-11-13 21:53:38 +0530

    state file logic

--- QA/Provider.tf
+++ QA/Provider.tf
@@ -4,9 +4,9 @@ region = "us-west-2"
 }
 
 terraform {
-backend "s3"{
-bucket = "enviornment-state-devops1"
-key = "QA/qa-state-file.tf"
-region = "us-west-2"
-}
+  backend "s3"{
+    bucket = "enviornment-state-devops1"
+    key = "QA/qa-state-file.tf"
+    region = "us-west-2"
+ }
 }
