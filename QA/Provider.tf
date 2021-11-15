a0309738840470bee144558928624bf7ba7f5861
Sanket, Wawale <sanket.wawale@gmail.com>
2021-11-15 22:04:49 +0530

    changing the bucket

--- QA/Provider.tf
+++ QA/Provider.tf
@@ -5,7 +5,7 @@ region = "us-west-2"
 
 terraform {
   backend "s3"{
-    bucket = "enviornment-state-devops1"
+    bucket = "enviornment-state2"
     key = "QA/qa-state-file.tf"
     region = "us-west-2"
  }
