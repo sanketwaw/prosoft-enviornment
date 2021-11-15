fc90f9e3d123429b8f118583f87359172d94eedf
Sanket, Wawale <sanket.wawale@gmail.com>
2021-11-15 23:00:57 +0530

    after adding security group

--- QA/main.tf
+++ QA/main.tf
@@ -3,9 +3,10 @@
 resource "aws_instance" "webserver" {
   ami           = "ami-0e5b6b6a9f3db6db8" 
   instance_type = "t2.micro"
-  key_name = "linux"
+  key_name = "Windows"
   subnet_id = "subnet-8fb4d0f7"
   count = 1
+  security_group_ids = "sg-0f30ecdf26ae644db"
   user_data = file("script.sh")
 
   tags = {
