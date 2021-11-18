29bd8bfb7fa06684a8edc1e9aa0e9f58d3c92d6a
Sanket, Wawale <sanket.wawale@gmail.com>
2021-11-18 22:55:03 +0530

    again adding key

--- QA/script.sh
+++ QA/script.sh
@@ -11,4 +11,4 @@ systemctl enable httpd
 ssh-keygen
 echo "<h1>This New Web server is created by Terra</h1>" > /var/www/html/index.html
 
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBB6qOP5632C6TnGI96oqn6It/cuiWRCFJSwp2bHJpoOAHeZWSdF8zzabaRwLTzYgOBEJjdmBjrgzm4IUG0L9mInM7zxR9nLcei1emrO+YQDVOtrm0mszluDccVO6Mr0xxP+cBF6WJCWfR3Z1kZjaPRY8QXo5U4SWjLKYLkI735ZW/mTZJfj5C+o1WvMYSKk57sYsz6o1oYizrg0X2XAIcGjMU2BrutVmW0B9vwyzaXgCF12vK/x5wWDP44ui2EmuMGhP2+K7/LTeNuCKrJz9CUOGN4gBLpdiwbIb3q6RFihLNdr8vyb22NyEBdSU8k61rvsQ5kabIoQALwUBzagv/ jenkins@ip-172-31-24-55.us-west-2.compute.internal" >> /root/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdShz5PbaXA1Iam8BZ+sg1CsGeaT3TT0fXRNR7TfLzqaXkk0WcX9LPLD5n0SS++g53WPngy50p9pwQhZMZ5S8GR6Bjog75XDc4iakE8sMBsIoERAGikAnVPL6OzoWmpFUdq+Q/tWLX1QHFG2q7ImfrUDl0gRUkkwwzE838IeSavEB4hgKwAYLx638aFQrEtD0D2LeQ6ljp95MtFu9qiSJO9ekBrMboJ/nWlLHvq59PyA01Z0MpSoXamtcmrIBZbJRG8ZMfJdeDJZcAGV3w/GbsRehPFNhR0IQ/R63aX8NpLryBLH2DULBu3oZS6KjP4pd4lSZr9AigCOyh3g1coRkj jenkins@ip-172-31-24-55.us-west-2.compute.internal" >> /root/.ssh/authorized_keys
